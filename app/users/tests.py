d3ee95f59f0c777f3d1f3fe4e6aa4e78185fc2b1
INSEOP KIM <inseop0813@gmail.com>
2024-04-28 16:44:50 +0900

    create custom user model

---
+++ app/users/tests.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+# TDD: Test Driven Development (테스트 주도 개발) 
+# => 채용공고 => 우대사항: TDD
+
+class UserTestCase(TestCase):
+    
+    # 일반 유저 생성 테스트 함수
+    def test_create_user(self):
+        email = 'inseop@gmail.com'
+        password = 'password123'
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        # 유저가 정상적으로 잘 만들어졌는지?
+        self.assertEqual(user.email, email)
+        # self.assertEqual(user.check_password(password), True)
+        self.assertTrue(user.check_password(password))
+        # self.assetEqual(user.is_superuser, False)
+        self.assertFalse(user.is_superuser)
+
+    # 슈퍼 유저 생성 테스트 함수
+    def test_create_superuser(self):
+        email = 'inseop_super@gmail.com'
+        password = 'password123'
+
+        user = get_user_model().objects.create_superuser(
+            email = email,
+            password = password
+        )
+
+        # 슈퍼유저
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+    # docker-compose run --rm app sh -c 'python manage.py makemigrations'
+    # docker-compose run --rm app sh -c 'python manage.py migrate'
+    # docker-compose run --rm app sh -c 'python manage.py test users'
